497af040ef421cb1b90a3032c2de5e25cfa0feb5
Christopher Oliphant <oliphant.christopher@gmail.com>
2014-11-05 07:06:43 -0500

    resign game

---
+++ public/javascripts/angularResignGame.js
@@ -0,0 +1,51 @@
+angular.module('handAndFoot')
+	.service('resignGameService', [
+		'$modal',
+		function($modal){
+			var modalDefaults = {
+				backdrop: true,
+				keyboard: true,
+				modalFade: true,
+				templateUrl: '/partials/resignGame.html'
+			};
+
+			var modalOptions = {
+				closeButtonText: 'Cancel',
+				actionButtonText: 'Continue',
+				headerText: 'Resign Game'
+			};
+
+			this.showModal = function (customModalDefaults, customModalOptions) {
+				if (!customModalDefaults) customModalDefaults = {};
+				customModalDefaults.backdrop = 'static';
+				return this.show(customModalDefaults, customModalOptions);
+			};
+
+			this.show = function (customModalDefaults, customModalOptions) {
+				//Create temp objects to work with since we're in a singleton service
+				var tempModalDefaults = {};
+				var tempModalOptions = {};
+
+				//Map angular-ui modal custom defaults to modal defaults defined in service
+				angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
+
+				//Map modal.html $scope custom properties to defaults defined in service
+				angular.extend(tempModalOptions, modalOptions, customModalOptions);
+
+				if (!tempModalDefaults.controller) {
+					tempModalDefaults.controller = function ($scope, $modalInstance) {
+						$scope.modalOptions = tempModalOptions;
+						$scope.modalOptions.ok = function (result) {
+							$modalInstance.close(result);
+						};
+						$scope.modalOptions.close = function (result) {
+		console.log(result);
+							$modalInstance.dismiss('cancel');
+						};
+					}
+				}
+
+				return $modal.open(tempModalDefaults).result;
+			};
+		}
+	]);
