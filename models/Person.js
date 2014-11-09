74bffc04bd2b2a77e497d05aee9b875b537cdaf3
Christopher Oliphant <oliphant.christopher@gmail.com>
2014-11-09 09:24:55 -0500

    improve stats

--- models/Person.js
+++ models/Person.js
@@ -4,24 +4,24 @@ var StatsSchema = new mongoose.Schema({
 	gameId: String,
 	gameName: String,
 	dateEnded: { type: Date, default: Date.now },
-	yourTeam: {
-		partner: {
+	yourTeam: [{
+		partner: [{
 			personId: String,
 			name:String
-		},
+		}],
 		score: Number
-	},
-	theirTeam: {
-		player1: {
+	}],
+	theirTeam: [{
+		player1: [{
 			personId: String,
 			name:String
-		},
-		player2: {
+		}],
+		player2: [{
 			personId: String,
 			name:String
-		},
+		}],
 		score: Number
-	}
+	}]
 });
 
 var PersonSchema = new mongoose.Schema({
