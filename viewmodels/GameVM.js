33ae9e946f0aed34b16428340fa10eb91234c4f9
Christopher Oliphant <oliphant.christopher@gmail.com>
2014-11-09 09:07:36 -0500

    improve stats

--- viewmodels/GameVM.js
+++ viewmodels/GameVM.js
@@ -444,8 +444,8 @@ var GameVM = function() {
 		var nsResigned = false;
 		var ewResigned = false;
 		if (personId) {
-			if (personId.toString() === game.nsTeam.players[0].person[0].toString()
-			|| personId.toString() === game.nsTeam.players[0].person[1].toString())
+			if (personId.toString() === game.players[0].person._id.toString()
+			|| personId.toString() === game.players[2].person._id.toString())
 				nsResigned = true;
 			else
 				ewResigned = true;
