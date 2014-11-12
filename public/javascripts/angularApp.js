74be84d2124932d1db05ceea935b159548bad9fb
Christopher Oliphant <oliphant.christopher@gmail.com>
2014-11-11 22:37:15 -0500

    fix bugs

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -134,3 +134,17 @@ angular.module('handAndFoot')
 			return result;
         };
     });
+angular.module('handAndFoot')
+	.directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.ngEnter, {'event': event});
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
